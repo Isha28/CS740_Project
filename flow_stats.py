0c5d896ac5645307b232ab9d27e7d9d57a7d592a
Isha28 <isha.p285@gmail.com>
2022-04-01 17:55:59 -0500

    TCP flow statistics added

--- flow_stats.py
+++ flow_stats.py
@@ -1,50 +1,71 @@
 import pyshark
 cap = pyshark.FileCapture('sample.pcap')
 #TODO: for live capture, use pyshark.LiveCapture
-sess_index = [] # to save stream indexes in an array
-flow_dict = {}
+
+udp_flow_dict = {}
+tcp_flow_dict = {}
+
+#Take count of packets
+total = 0
+udp = 0
+tcp = 0
+
+def add_to_flow_dict(flow_dict,flow_id,pkt):
+    if flow_id not in flow_dict:
+        flow_dict[flow_id] = []
+    if flow_id in flow_dict:
+        flow_dict[flow_id].append(pkt)
+
+def flow_statistics(flow_dict):
+    flow_stats = {}
+    for idx in range(len(flow_dict)):
+        total_len = 0
+        total_pkt = 0
+
+        for packet in flow_dict[idx]:
+            total_len += int(packet.length)
+            total_pkt += 1
+
+        flow_stats[idx] = {}
+        flow_stats[idx]["avg_len"] = total_len / total_pkt
+        flow_stats[idx]["total_pkt"] = total_pkt
+        flow_stats[idx]["total_len"] = total_len
+
+    return (len(flow_stats))
+
 for pkt in cap:
+    total += 1
     try:
-        # TODO: capturing only for udp stream - need to do for other streams like tcp
-        sess_index.append(pkt.udp.stream)
-        flow_id = int (pkt.udp.stream)
-        if flow_id not in flow_dict:
-            flow_dict[flow_id] = []
-        if flow_id in flow_dict:
-             flow_dict[flow_id].append(pkt)
+        if "UDP" in pkt and pkt.udp.stream != 0:
+            udp += 1
+            flow_id = int (pkt.udp.stream)
+            add_to_flow_dict(udp_flow_dict, flow_id, pkt)
+
+        elif "TCP" in pkt and pkt.tcp.stream != 0:
+            tcp += 1
+            flow_id = int (pkt.tcp.stream)
+            add_to_flow_dict(tcp_flow_dict, flow_id, pkt)
+
+        # Other streams not needed - Follow stream disabled for others in Wireshark
+
     except:
         pass
 
+print ("total packets ", total)
+print ("udp packets ", udp)
+print ("tcp packets ", tcp)
+
 # TODO: need to get for all flow statistics
+print ("UDP flow statistics ", flow_statistics(udp_flow_dict))
+print ("TCP flow statistics ", flow_statistics(tcp_flow_dict))
 
-flow_stats = {}
-for idx in range(len(flow_dict)):
-    total_len = 0
-    total_pkt = 0
-    for packet in flow_dict[idx]:
-        total_len += int(packet.length)
-        total_pkt += 1
-    flow_stats[idx] = {}
-    flow_stats[idx]["avg_len"] = total_len/total_pkt
-    flow_stats[idx]["total_pkt"] = total_pkt
-    flow_stats[idx]["total_len"] = total_len
+#print (packet.length)
+#print (packet.transport_layer)
+#print (packet.ip.src)
+#print (packet.ip.dst)
+#print (packet[packet.transport_layer].srcport)
+#print (packet[packet.transport_layer].dstport)
+#print (packet.eth.src)
+#print (packet.eth.dst)
 
-print (flow_stats[3])
 
-    #print (packet.length)
-    #print (packet.transport_layer)
-    #print (packet.ip.src)
-    #print (packet.ip.dst)
-    #print (packet[packet.transport_layer].srcport)
-    #print (packet[packet.transport_layer].dstport)
-    #print (packet.eth.src)
-    #print (packet.eth.dst)
-    #print ("###################")
-
-#print (sess_index)
-#if len(sess_index) == 0:
-#    max_index = 0
-#    print ("No TCP Found")
-#else:
-#    max_index = int(max(sess_index)) + 1 # max function is used to get the highiest number
-#print (max_index)
